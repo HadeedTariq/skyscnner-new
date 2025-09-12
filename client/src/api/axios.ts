87e2da1716d8736866492aa859569a01a1cb6df0
HadeedTariq <computeranalog351@gmail.com>
2025-09-12 16:01:32 +0500

    make some changes in axios file for production

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL:
     import.meta.env.VITE_ENV === "development"
       ? "http://localhost:3001/api"
-      : "https://sky-scanner-backend.vercel.app/api",
+      : "https://easyfligth/api/api",
   headers: {
     "Content-Type": "application/json",
   },
